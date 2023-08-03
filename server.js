86abe4150cc3630eae3f85780c3747e86998dc27
James Matthew Haynes <jamesmatthewhaynes@jamess-imac.myfiosgateway.com>
2023-08-03 13:03:00 -0400

    added basic routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const noteData = require('./db/db.json');
 
 const PORT = process.env.port || 3001;
 
@@ -9,3 +10,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(noteData);
+});
+
+app.post('/api/notes', (req, res) => {
+    
+});
+
+
+
+
+app.listen(PORT, () => 
+    console.log(`App listening at http://localhost:${PORT}`)
+);
