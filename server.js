c469af453a6effde5aa0f0733593a1e7ae93decf
James Matthew Haynes <jamesmatthewhaynes@jamess-imac.myfiosgateway.com>
2023-08-03 14:39:22 -0400

    v2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const apiRoutes = require("./routes/apiRoutes")
 const htmlRoutes = require("./routes/htmlRoutes")
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
